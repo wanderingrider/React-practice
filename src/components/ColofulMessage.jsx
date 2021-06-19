903d34e5f01653d8c001efc5256338264a31a448
wanderingrider <78330447+wanderingrider@users.noreply.github.com>
2021-06-19 10:56:27 +0900

    export　change

--- src/components/ColofulMessage.jsx
+++ src/components/ColofulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColofulMessage = (props) => {
+export const ColofulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     color,
@@ -8,5 +8,3 @@ const ColofulMessage = (props) => {
   };
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default ColofulMessage;
